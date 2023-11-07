14a692da8d87eb656f3160b4a0ef21ba6f80bbfe
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2023-11-08 00:31:06 +0900

    baekjoon in C

---
+++ leeD/C/ProjectJ&K/ProjectJ&K/19532.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int arr[6];
+	int x, y;
+
+	for (int i = 0; i < 6; i++) {
+		scanf("%d", &arr[i]);
+	}
+	//if (arr[0] > 0 && arr[3] > 0)
+	
+}
