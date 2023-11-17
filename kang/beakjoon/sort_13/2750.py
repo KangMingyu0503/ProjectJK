0535c3ebdb8f7f6c19f3dfbe5e84cab6f97d58de
KangMingyu0503 <alexkang0503@gmail.com>
2023-11-17 21:02:21 +0900

    2750is done

--- kang/beakjoon/sort_13/2750.py
+++ kang/beakjoon/sort_13/2750.py
@@ -1,13 +1,13 @@
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(int(input()))
-arr.sort()
-for j in range(n):
-    print(arr[j])
-
 # n = int(input())
-# arr = [int(input()) for i in range(n)]
+# arr = []
+# for i in range(n):
+#     arr.append(int(input()))
 # arr.sort()
 # for j in range(n):
 #     print(arr[j])
+
+n = int(input())
+arr = [int(input()) for i in range(n)]
+arr.sort()
+for j in range(n):
+    print(arr[j])
