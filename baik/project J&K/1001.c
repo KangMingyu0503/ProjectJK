ce2a1c26b029bc410bc36898dff2be5eb9aaf392
CE_BAIKJUWON <75535083+BAIKJUWON@users.noreply.github.com>
2023-12-04 19:54:07 +0900

    주석 업데이트 12/04

--- baik/project J&K/1001.c
+++ baik/project J&K/1001.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //C언어를 사용하기 위한 헤더파일
 
-int main(){
-    printf("Hello");
-    return 0;
+int main(){ // 메인함수(C언어에서 가장 먼저 실행이 된다.)
+    printf("Hello"); //출력문 : Hello라는 문자를 출력한다.
+    return 0; //0을 반환해준다.
 }
