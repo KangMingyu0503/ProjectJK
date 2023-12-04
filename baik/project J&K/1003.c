7907595e72e1c349198512aa518b6abe1312bc2f
CE_BAIKJUWON <75535083+BAIKJUWON@users.noreply.github.com>
2023-12-04 19:55:20 +0900

    주석 업데이트 12/04

--- baik/project J&K/1003.c
+++ baik/project J&K/1003.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //C언어를 사용하기 위한 헤더파일
 
-int main(){
-    printf("Hello\nWorld");
-    return 0;
+int main(){ // 메인함수(C언어에서 가장 먼저 실행이 된다.)
+    printf("Hello\World"); //\n : 줄바꿈
+    return 0; //0을 반환해준다.
 }
